54e0bbe779de8ee4bc531a13ae678a8246f6e510
h4niz <30542876+H4niz@users.noreply.github.com>
2023-07-09 22:36:22 +0700

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
-__API_TOKEN__ = "1457372735:AAH1Evy_Ti5vwiLLeMigvymvaOoWINhUMqc"
-__LOG_PATH__ = r"C:\Users\anhnlq\Documents\GitHub\AssistantBot\Logs"
-__WORKSPACE_DIR__ = r"C:\Users\anhnlq\Documents\GitHub\AssistantBot\Bond"
+__API_TOKEN__ = ":"
+__LOG_PATH__ = r"C:\Users\\Documents\GitHub\AssistantBot\Logs"
+__WORKSPACE_DIR__ = r"C:\Users\\Documents\GitHub\AssistantBot\Bond"
 _USE_PROXY_ = True
-_PROXY_FILE_ = r"C:\Users\anhnlq\Documents\GitHub\AssistantBot\proxy.conf"
+_PROXY_FILE_ = r"C:\Users\\Documents\GitHub\AssistantBot\proxy.conf"
